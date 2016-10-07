e7f3a19a029e943f8aa071abe9983ab6ac8fa6ca
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-10-07 17:43:08 -0400

    Making my method that prints the input backwards

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -3,24 +3,29 @@ package doublelinkedlist;
 public class DoubleLinkedList {
     //Node class
     class Node    {
-          //String 
-        String data;
-        //Node variables
-        Node next;
-        Node previous;
-        //Node constructor
+            //String 
+            String data;
+            //Node variables
+            Node next;
+            Node previous;
+            //Node constructor
         public Node(String d)
                 {
-        //defining variables
-        data = d;
-        next = null;
-        previous = null;
+            //defining variables
+            data = d;
+            next = null;
+            previous = null;
                     
                 }
     }
+    //Method that prints the inpute backwards
+        void printBack()
+        {
+            
+        }
  //Main class
     public static void main(String[] args) {
        
     }
-  
+
 }
