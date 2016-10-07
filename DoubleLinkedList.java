3e5db4bc93bbb795d10c8f754301ddff11b3b6a9
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-10-07 17:30:39 -0400

    Making the constructor for my Node class

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,12 +1,22 @@
 package doublelinkedlist;
 //The Double Linked List class
 public class DoubleLinkedList {
-    //Making my node class
-    class Node
-    {
-        //String variables
+    //Node class
+    class Node    {
+          //String 
         String data;
-        
+        //Node variables
+        Node next;
+        Node previous;
+        //Node constructor
+        public Node(String d)
+                {
+        //defining variables
+        data = d;
+        next = null;
+        previous = null;
+                    
+                }
     }
  //Main class
     public static void main(String[] args) {
