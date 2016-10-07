11e3d1b1a1c4e80e25255442ab7c324416cad6a4
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-10-07 17:11:01 -0400

    Making my node class

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,10 +1,14 @@
 package doublelinkedlist;
-
+//The Double Linked List class
 public class DoubleLinkedList {
-
- 
+    //Making my node class
+    class Node
+    {
+        
+    }
+ //Main class
     public static void main(String[] args) {
        
     }
-    
+  
 }
