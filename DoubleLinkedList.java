36dd32ef161670e35346bda25cb6091b1efd94d2
DESKTOP-UGN8TA5\Emoney <ethornton@albany.edu>
2016-10-19 16:26:15 -0400

    Working on the delete node method more

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,14 +1,14 @@
 
 package doublelinkedlist;
 
-import java.util.*;
 public class DoubleLinkedList 
 {
 
     
     Node head;
-    Node current;
+    Node tail;
     Node temp;
+    
     //Node Class
     class Node
     {
@@ -25,144 +25,66 @@ public class DoubleLinkedList
         }
     }
 
-    public void add(String data)
+    public void add(String str)
     {
-       Node newNode = new Node(data);
+      Node newNode = new Node(str);
        
-       
-        if(this.isEmpty())
-       {
-          this.head = newNode;
-       } 
-        else
-       {
-           current = this.head;
-           
-           while(newNode.data.compareTo(current.data) < 0)           // while(current.next != null)
-          {
-               current = current.next;
-               
-               
-               newNode = current.next;
-               
-               
-          }
-              
-           current.next = newNode;
-           newNode.prev = current;
-           newNode.next = null;
-       }
-       
+       if(head == null)
+            {
+                head =  new Node(str);
+                tail = head;
+            }
+                else
+               {
+                tail.next = new Node(str);
+                tail = tail.next;
+               }
         
-        int result = newNode.data.compareTo(this.head.data);
-        System.out.println(result);
-        if (newNode.data.compareTo(this.head.data) < 0) 
-        {
-            
-        }
-    }
     
-  
-    public boolean isEmpty()   
-    {
-        return this.head == null;
     }
+     public boolean isEmpty() {
+    
+    return this.head == null;
+  }
     
     public void print()
     {
-        current = this.head;
-        while(current != null)
+        temp = this.head;
+        while(temp != null)
         {     
-            System.out.print(current.data + "--");
-            current = current.next;
+            System.out.print(temp.data +" " );
+            temp = temp.next;
         }
-        System.out.print(">NULL");
     }
    
     //Backwards print method
     public void printBack()
     {
-        temp = null;
-        current = this.head;
-            
-        while(current != null)
-        {     
-            temp = current.prev;
-            current.prev = current.next;
-            current.next = temp;
-            current = current.prev;
-        }
-        if(temp != null)
-        {
-            head = temp.prev;
-        }
-    }
-    
-    public void delete(int nodeNum)
-    {
-        int i = 1;
-        current = head;
-        
-        if(nodeNum == 1)
-        {
-            head = current.next;
-            current.next = null;
-            current.prev = null;
-        }
-        else
-        {
-            while(i != nodeNum)
+              
+            temp = this.tail;
+            while(temp != null)
             {
-                current = current.next;
-                
-                i++;  
+                System.out.print(temp.data + " ");
+                temp = temp.prev;
             }
-            if(current.next == null)
-            {
-                current.prev.next = null;
-                current.prev = null;
-            }
-            else
-            {
-                current.prev.next = current.next;
-                current.next.prev = current.prev;
-            }
-        }
     }
     
     public static void main(String[] args) 
     {
-        DoubleLinkedList list = new DoubleLinkedList();
+        DoubleLinkedList Llist = new DoubleLinkedList();
         
-        list.add("apple");
-        list.add("orange");
-        list.add("banana");
-        list.add("kiwi");
-        list.add("grape");
-        
-        int deleteVal = 4;
-        
-        System.out.println("The List is: ");
-        list.print();
-        
-        System.out.println("\n-------------------------------------------");
-        
-        System.out.println("The Backwards List is: ");
-        list.printBack();
-        list.print();
-        
-        System.out.println("\n-------------------------------------------");
-       
-        list.printBack();
-        
-        System.out.println("Now to delete node " + deleteVal + " from the list...");
+       Llist.add("fruit");
+       Llist.add("orange");
+       Llist.add("banana");
        
         
-        list.delete(deleteVal);
-        System.out.println("The New List is: ");
-        list.print();
-        System.out.println("\n-------------------------------------------");
+        Llist.print();
+        System.out.println("");
+        Llist.printBack();
        
+       
+     
+        
 
        // Scanner scan = new Scanner(System.in);
        // System.out.print("Enter a node to search the list. If it returns true it's in the list: ");
@@ -183,6 +105,41 @@ public class DoubleLinkedList
            // list.print();
             nodeNum --;
         } */
+        
+        //sort
+        /*   Node newNode = new Node(str);
+       
+       
+        if(this.isEmpty())
+       {
+          this.head = newNode;
+       } 
+        else
+       {
+           tail = this.head;
+           
+           while(newNode.data.compareTo(tail.data) < 0)          
+          {
+               tail = tail.next;
+               
+               
+               newNode = tail.next;
+               
+               
+          }
+              
+           tail.next = newNode;
+           newNode.prev = tail;
+           newNode.next = null;
+       }
+       
+        
+        int result = newNode.data.compareTo(this.head.data);
+        System.out.println(result);
+        if (newNode.data.compareTo(this.head.data) < 0) 
+        {
+            
+        }*/
     }
     
 }
